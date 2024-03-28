da680edc13674e1cb92dbf40025b53ea01981511
Javokhir1105 <161455530+Javokhir1105@users.noreply.github.com>
2024-03-28 18:15:27 +0500

    index.js and index.html

---
+++ index.js
@@ -0,0 +1,181 @@
+// let x=15
+// let y=5
+// console.log(x+y)
+
+// let R=5
+// let L=2*Math.PI*R
+// let S=Math.PI*R*R
+// console.log(S,L)
+
+// let a=5
+// let b=125
+// let G=Math.sqrt(a*b)
+// console.log(G)
+
+// let x=5
+// let y=3*Math.pow(x,6)-6*Math.pow(x,2)-7
+// console.log(y)
+
+// let X=3
+// let A=21000
+// let Y=5
+// let S=(A/X)*1
+// let F=(A/X)*Y
+// console.log(S,F)
+
+// let A=10
+// let B=23
+// let S=A+12
+// let F=B+12
+// console.log(S,F)
+
+// let X=27
+// let S=Math.floor(X/10)
+// let F=X%10
+// let R=S+F
+// console.log(F,S,R)
+
+// let X=27 
+// let S=Math.floor(X/10)
+// let F=X%10
+// let D="7"
+// let R=S
+// console.log(D+R)
+
+// let X=569
+// let S=(X-X%100)/100
+// console.log(S)
+
+// let X=1269
+// let S=(X-(X-X%1000)-X%100)/100
+// console.log(S)
+
+// let a=5
+// let b=7
+// let P=2(a+b)
+// let S=a*b
+// console.log(S,P)
+ 
+// let d=12
+// let r=d/2
+// let L=Math.PI*d
+// let S=Math.PI*Math.pow(r,2)
+// console.log(L,S,r)
+
+// let a=6
+// let V=Math.pow(a,3)
+// let S=6*a*a
+// console.log(S,V)
+
+// let a=4
+// let b=9
+// let c=6
+// let V=a*b*c
+// let S=2*(a*b+a*c+b*c)
+// console.log(V,S)
+
+// let a=8
+// let b=14
+// let L=(a+b)/2
+// console.log(L)
+
+// let a=3
+// let b=4
+// let c=Math.sqrt(a*a+b*b)
+// let P=a+b+c
+// console.log(P,c)
+
+// let r1=5
+// let r2=7
+// let s1=Math.PI*r1*r1
+// let s2=Math.PI*r2*r2
+// let s=Math.PI*(r1*r1+r2*r2)
+// console.log(s,s1.s2)
+
+// let x1=4
+// let x2=3
+// let y1=6
+// let y2=5
+// let M=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
+// console.log(M)
+
+// let x=5
+// let y=4*Math.pow(x-3,6)-7*Math.pow(x-3,3)+2
+// console.log(y)
+
+// let Tf=47
+// let Tc=(Tf-32)*5/9
+// console.log(Tc)
+
+// let X=4
+// let A=24000
+// let Y=5
+// let B=10000
+// let S=A/X
+// let F=B/Y
+// let Z=S-F
+// console.log(S,F,Z)
+
+// let X=3
+// let A=4700
+// let Y=7
+// let B=3400
+// let S=X*A
+// let F=Y*B
+// let Z=F+S
+// console.log(S,F,Z)
+
+// let x=692
+// let s=x%10
+// let f=((x-x%10)%100)/10
+// console.log(s,f)
+
+// let x=324
+// let a=x%10
+// let b=((x-x%10)%100)/10
+// let c=(x-x%100)/100
+// let s=a+b+c
+// console.log(s)
+
+// let x=324
+// let a=x%10
+// let b=((x-x%10)%100)/10
+// let c=(x-x%100)/100
+// let s=a*100+b*10+c
+// console.log(s)
+
+// let x=324
+// let a=x%10
+// let b=((x-x%10)%100)/10
+// let c=(x-x%100)/100
+// let s=b*100+c*10+a
+// console.log(s)
+
+// let x=324
+// let a=x%10
+// let b=((x-x%10)%100)/10
+// let c=(x-x%100)/100
+// let s=c*100+a*10+b
+// console.log(s)
+
+// let x=56789
+// let f=((x-x%1000)-(x-x%10000))/1000
+// console.log(f)
+
+// let s=3786
+// let d=Math.floor(s/60)
+// console.log(d)
+
+// let s=37986
+// let h=Math.floor(s/3600)
+// console.log(h)
+
+// let s=3786
+// let d=Math.floor(s/60)
+// let f=s%60
+// console.log(d,f)
+
+// let s=3786
+// let d=Math.floor(s/3600)
+// let f=s%3600
+// console.log(d,f)
